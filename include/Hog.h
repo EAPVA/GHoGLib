b4d4f05819b0af83207592a98877b6bdbe7ac7fc
Marcelo Teider Lopes <teiderlopes@gmail.com>
2015-05-12 16:56:09 -0300

    Add main GHogLib API

---
+++ include/Hog.h
@@ -0,0 +1,59 @@
+/*
+ * Hog.h
+ *
+ *  Created on: May 12, 2015
+ *      Author: marcelo
+ */
+
+#ifndef HOG_H_
+#define HOG_H_
+
+#include <vector>
+#include <string>
+
+#include <include/MultilayerPerceptron.h>
+
+#include <include/GHogLibConstants.h>
+
+namespace ghog
+{
+namespace lib
+{
+
+class HogCallback
+{
+public:
+	virtual ~HogCallback() = 0;
+	virtual void operator()(std::vector< cv::Rect > found_objects) = 0;
+};
+
+class Hog
+{
+public:
+	Hog(HogCallback* callback,
+		std::string settings_file);
+	virtual ~Hog();
+
+	GHOG_LIB_STATUS locate(cv::Mat img,
+		cv::Rect roi = _roi,
+		cv::Size window_size = _window_size,
+		cv::Size window_stride = _window_stride);
+
+protected:
+	MultilayerPerceptron _mlp;
+	HogCallback* _callback;
+	Settings _settings;
+
+	cv::Rect _roi;
+	cv::Size _window_size; // In pixels
+	cv::Size _window_stride; // In pixels
+
+	int _num_bins;
+	cv::Size _grid_size; // In number of cells
+	cv::Size _block_size; // In number of cells
+	cv::Size _block_stride; // In number of cells
+};
+
+} /* namespace lib */
+} /* namespace ghog */
+#endif /* HOG_H_ */
---
+++ src/Hog.cpp
@@ -0,0 +1,33 @@
+/*
+ * Hog.cpp
+ *
+ *  Created on: May 12, 2015
+ *      Author: marcelo
+ */
+
+#include <include/Hog.h>
+
+namespace ghog
+{
+namespace lib
+{
+
+Hog::Hog(HogCallback* callback,
+	std::string settings_file) :
+	_callback(callback),
+	_settings(settings_file)
+{
+	_mlp(cv::Mat());
+	_num_bins = 0;
+	_grid_size = cv::Size(0,0);
+	_block_size = cv::Size(0,0);
+	_block_stride = cv::Size(0,0);
+}
+
+Hog::~Hog()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace lib */
+} /* namespace ghog */
